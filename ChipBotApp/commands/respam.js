730a1b90c340fc01313d9cebef67d2db21716bbc
Lewis Hitching <lewishitching@yahoo.co.uk>
2020-05-05 13:48:39 +0100

    added respam command

---
+++ ChipBotApp/commands/respam.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'respam',
+    usage: '<messsage-count>',
+    description: 'Throw me a number, I\'ll count backwards.',
+    execute(message, args) {
+        var deleteCount = parseInt(args[0]);
+        if (!args.length) {
+            message.channel.send('You didn\'t specify a number');
+        } else {
+            if (!isNaN(deleteCount)) {
+                message.channel.bulkDelete(deleteCount).then(() => {
+                    message.channel.send("Deleted "+args[0]+" messages.").then(msg => msg.delete({ timeout: 3000 })).catch(function () { console.log("Timeout delete failed")});
+                }).catch(function () {console.log("Promise Rejected")});
+            } else {
+                message.channel.send('You didn\'t specify a number');
+            }
+        }
+    },
+};
--- ChipBotApp/index.js
+++ ChipBotApp/index.js
@@ -7,7 +7,6 @@ const { prefix, version, defaultCommandCooldown, adminRoles } = require('./comma
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
