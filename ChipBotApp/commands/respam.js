776fe7da5a054a30a4214bc79478a00dfbd00af1
Lewis Hitching <lewishitching@yahoo.co.uk>
2020-05-05 23:49:59 +0100

    added maxDeletions commandvar to limit respam command, modified respam command to check against it and delete in increments if the arg is over 100

--- ChipBotApp/commands/respam.js
+++ ChipBotApp/commands/respam.js
@@ -1,3 +1,5 @@
+const { maxDeletions } = require('../commandVars.json');
+
 module.exports = {
     name: 'respam',
     usage: '<messsage-count>',
@@ -9,9 +11,32 @@ module.exports = {
             message.channel.send('You didn\'t specify a number');
         } else {
             if (!isNaN(deleteCount)) {
-                message.channel.bulkDelete(deleteCount).then(() => {
-                    message.channel.send("Deleted "+args[0]+" messages.").then(msg => msg.delete({ timeout: 3000 })).catch(function () { console.log("Timeout delete failed")});
-                }).catch(function () {console.log("Promise Rejected")});
+                var limited = "";
+                if (deleteCount > maxDeletions) {
+                    deleteCount = maxDeletions;
+                    limited = " (Max Limited)";
+                }
+
+                if (deleteCount <= 100) {
+                    message.channel.bulkDelete(deleteCount).then(() => {
+                        message.channel.send("Deleted " + deleteCount + " messages."+limited).then(msg => msg.delete({ timeout: 3000 })).catch(function () { console.log("Timeout delete failed") });
+                    }).catch(function () { console.log("Message deletion failed") });
+                } else {
+                    var i = deleteCount;
+                    while (i > 0) {
+                        minusedVal = i - 100;
+                        if (!minusedVal <= 0) {
+                            message.channel.bulkDelete(100).catch(function () { console.log("Old messages cannot be deleted") });;
+                            i = i - 100;
+                        } else {
+                            message.channel.bulkDelete(i).catch(function () { console.log("Old messages cannot be deleted") }).then(() => {
+                                message.channel.send("Deleted " + deleteCount + " messages." + limited).then(msg => msg.delete({ timeout: 3000 })).catch(function () { console.log("Timeout delete failed") });
+                            }).catch(function () { console.log("Message deletion failed") });
+                            i = i - i;
+                        }
+                    }
+                }
+
             } else {
                 message.channel.send('You didn\'t specify a number');
             }
